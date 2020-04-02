6377f3a4079687b03c0cc41be7db9ce82ab06c9e
Wongani Kaluwa <kaluawongan@gmail.com>
2020-04-02 14:40:06 +0200

    Add files via upload

---
+++ KodiStudioEngine/src/Compiler/Compiler.java
@@ -0,0 +1,16 @@
+package Compiler;
+
+/**
+ *
+ * @author acer
+ */
+public class Compiler {
+    public void compileScript(String scriptLocation){
+    //compile and translate script inside this code
+    
+    }
+    
+    public void runProject(String projectLocation){
+    //run project from location
+    }
+}
